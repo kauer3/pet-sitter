0b028bf4cef902de8a300fdb82c2e31ca9f37419
Kauê Reis <kaue0607@gmail.com>
2025-09-18 22:32:26 +1200

    Update footer message

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center pt-20 pb-20 gap-12">
       <div className="flex flex-col gap-6 mx-5 max-w-2xs sm:max-w-2xl md:max-w-3xl lg:max-w-5xl text-center">
-        <p className="text-lg text-text-900 leading-relaxed italic grow-2">
-          I'm always happy to hear from you! Whether you have questions about my services, want to discuss your pet's needs, or just want to say hello, feel free to reach out. You can contact me via email at <a href="mailto:julia505@gmail.com" className="text-primary-700 hover:underline">julia505@gmail.com</a> or connect with me on social media. I look forward to hearing from you!
+        <p className="text-lg text-justify text-text-900 leading-relaxed italic grow-2">
+          I'm always happy to hear from you! Whether you have questions about my services, want to discuss your pet's needs, or just want to say hello, feel free to reach out. You can send me a message thorugh the form above, contact me via email at <a href="mailto:julia505@gmail.com" className="text-primary-700 hover:underline">julia505@gmail.com</a> or connect with me on social media. I look forward to hearing from you!
 </p>
         <div className="flex flex-row gap-4 justify-center items-center align-middle text-text-700 leading-relaxed">
           <a href="https://www.instagram.com/julia_vetcare/" target="_blank" rel="noopener noreferrer" className="text-primary-700 hover:text-primary-700">
