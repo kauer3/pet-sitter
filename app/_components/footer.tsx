ba1e43dde035af2ee469aa26c9b003f306ae35aa
Kauê Reis <kaue0607@gmail.com>
2025-10-02 23:45:25 +1300

    Update footer links

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -5,13 +5,13 @@ export default function Footer() {
     <footer className="flex flex-col items-center justify-center pt-20 pb-20 gap-12">
       <div className="flex flex-col gap-6 mx-5 max-w-2xs sm:max-w-2xl md:max-w-3xl lg:max-w-5xl text-center">
         <p className="text-lg text-justify text-text-900 leading-relaxed italic grow-2">
-          I&apos;m always happy to hear from you! Whether you have questions about my services, want to discuss your pet&apos;s needs, or just want to say hello, feel free to reach out. You can send me a message thorugh the form above, contact me via email at <a href="mailto:julia505@gmail.com" className="text-primary-700 hover:underline">julia505@gmail.com</a> or connect with me on social media. I look forward to hearing from you!
+          I&apos;m always happy to hear from you! Whether you have questions about my services, want to discuss your pet&apos;s needs, or just want to say hello, feel free to reach out. You can send me a message thorugh the form above, contact me via email at <a href="mailto:juliareis505@gmail.com" className="text-primary-700 hover:underline">juliareis505@gmail.com</a> or connect with me on social media. I look forward to hearing from you!
 </p>
         <div className="flex flex-row gap-4 justify-center items-center align-middle text-text-700 leading-relaxed">
-          <a href="https://www.instagram.com/julia_vetcare/" target="_blank" rel="noopener noreferrer" className="text-primary-700 hover:text-primary-700">
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="text-primary-700 hover:text-primary-700">
             <Instagram size={32} />
           </a>
-          <a href="https://www.linkedin.com/in/júlia-silva-505/" target="_blank" rel="noopener noreferrer" className="text-primary-700 hover:text-primary-700">
+          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className="text-primary-700 hover:text-primary-700">
             <Linkedin size={32} />
           </a>
         </div>
